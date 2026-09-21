c61215c032945eb37e5c47172b6b2fbfea940ef0
Noob <akhilkrishnaus1234@gmail.com>
2026-09-21 16:52:15 +0530

    Add files via upload

---
+++ app/bot/callbacks/__init__.py
@@ -0,0 +1,3 @@
+from .router import navigation_callbacks
+
+__all__ = ["navigation_callbacks"]
---
+++ app/bot/callbacks/router.py
@@ -0,0 +1,3 @@
+from app.bot.handlers.callbacks import navigation_callbacks
+
+__all__ = ["navigation_callbacks"]
